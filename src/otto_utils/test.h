b9fd579cafcf323714911f74599506362e7907ff
Cristóbal Allendes <callendes.molina@gmail.com>
2023-09-14 00:01:42 -0300

    Made the test macros be in uppercase, and added a macro to call the tests

--- src/otto_utils/test.h
+++ src/otto_utils/test.h
@@ -5,29 +5,37 @@
 #define TEST_FAIL 1
 #define TEST_PASS 0
 
-#define otto_assert(cond)                                                      \
+/* Assert macros */
+
+#define OTTO_ASSERT(cond)                                                      \
   if (!(cond))                                                                 \
   return TEST_FAIL
-#define otto_assert_false(cond)                                                \
+#define OTTO_ASSERT_FALSE(cond)                                                \
   if (cond)                                                                    \
   return TEST_FAIL
-#define otto_assert_eqi(v1, v2)                                                \
+#define OTTO_ASSERT_EQI(v1, v2)                                                \
   if ((v1) != (v2)) {                                                          \
     log_debug("TEST_FAIL: Expected %i, found %i", v2, v1);                     \
     return TEST_FAIL;                                                          \
   }
-#define otto_assert_nei(v1, v2)                                                \
+#define OTTO_ASSERT_NEI(v1, v2)                                                \
   if ((v1) == (v2)) {                                                          \
     log_debug("TEST_FAIL: Expected different than %i, found %i", v2, v1);      \
     return TEST_FAIL;                                                          \
   }
-#define otto_assert_streq(v1, v2)                                              \
+#define OTTO_ASSERT_STREQ(v1, v2)                                              \
   if (strcmp((v1), (v2))) {                                                    \
     log_debug("TEST_FAIL: Expected %s, found %s", v2, v1);                     \
     return TEST_FAIL;                                                          \
   }
-#define otto_assert_strne(v1, v2)                                              \
+#define OTTO_ASSERT_STRNE(v1, v2)                                              \
   if (!strcmp((v1), (v2))) {                                                   \
     log_debug("TEST_FAIL: Expected different than %s, found %s", v2, v1);      \
     return TEST_FAIL;                                                          \
   }
+
+/* Test utilities */
+
+#define OTTO_CALL_TEST(test)                                                   \
+  if (test())                                                                  \
+    return TEST_FAIL;
