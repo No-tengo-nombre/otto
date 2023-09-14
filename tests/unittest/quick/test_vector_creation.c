0bdb99b18967a72fb6abe2fe643bcdd6368011ee
Cristóbal Allendes <callendes.molina@gmail.com>
2023-09-14 00:31:13 -0300

    Added basic tests for all the creation functions for vectors2

--- tests/unittest/quick/test_vector_creation.c
+++ tests/unittest/quick/test_vector_creation.c
@@ -4,7 +4,61 @@
 #include <otto/status.h>
 #include <otto/vector.h>
 
+#include "otto/devices.h"
 #include "src/otto_utils/test.h"
+
+int test_new() {
+  otto_vector_t vec;
+  otto_status_t status = OTTO_STATUS_SUCCESS;
+
+  status &= otto_vector_new(sizeof(uint32_t), &vec);
+
+  if (status == OTTO_STATUS_FAILURE) {
+    return TEST_FAIL;
+  }
+
+  if (vec.data != NULL) {
+    return TEST_FAIL;
+  }
+  if (vec.capacity != 0) {
+    return TEST_FAIL;
+  }
+  if (vec.device != OTTO_DEVICE_CPU) {
+    return TEST_FAIL;
+  }
+  if (vec.size != 0) {
+    return TEST_FAIL;
+  }
+  return TEST_PASS;
+}
+
+int test_with_capacity() {
+  otto_vector_t vec;
+  otto_status_t status = OTTO_STATUS_SUCCESS;
+
+  status &= otto_vector_with_capacity(6, sizeof(uint32_t), &vec);
+
+  if (status == OTTO_STATUS_FAILURE) {
+    return TEST_FAIL;
+  }
+
+  if (vec.data == NULL) {
+    return TEST_FAIL;
+  }
+  if (vec.capacity == 0) {
+    return TEST_FAIL;
+  }
+  if (vec.data_size != sizeof(uint32_t)) {
+    return TEST_FAIL;
+  }
+  if (vec.device != OTTO_DEVICE_CPU) {
+    return TEST_FAIL;
+  }
+  if (vec.size != 0) {
+    return TEST_FAIL;
+  }
+  return TEST_PASS;
+}
 
 int test_from_array() {
   otto_vector_t vec;
@@ -13,6 +67,7 @@ int test_from_array() {
   otto_status_t status = OTTO_STATUS_SUCCESS;
 
   status &= otto_vector_from_array(data, len, sizeof(uint32_t), &vec);
+
   if (status == OTTO_STATUS_FAILURE) {
     return TEST_FAIL;
   }
@@ -23,11 +78,16 @@ int test_from_array() {
       return TEST_FAIL;
     }
   }
+  if (vec.device != OTTO_DEVICE_CPU) {
+    return TEST_FAIL;
+  }
 
   return TEST_PASS;
 }
 
 int main() {
+  OTTO_CALL_TEST(test_new);
+  OTTO_CALL_TEST(test_with_capacity);
   OTTO_CALL_TEST(test_from_array);
   return TEST_PASS;
 }
