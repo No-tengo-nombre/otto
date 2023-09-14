d7ff1924c64dd7253bed6538ca998b0221e59720
Cristóbal Allendes <callendes.molina@gmail.com>
2023-09-14 17:09:28 -0300

    Added a test for extend_array

--- tests/unittest/quick/test_vector_modification.c
+++ tests/unittest/quick/test_vector_modification.c
@@ -48,6 +48,50 @@ int test_push() {
   return TEST_PASS;
 }
 
+int test_extend_array() {
+  log_info("Testing `extend_array`");
+  otto_vector_t vec;
+  uint32_t data[] = {0, 1, 2, 3, 4, 5, 6, 7};
+  size_t len = 8;
+  otto_status_t status = OTTO_STATUS_SUCCESS;
+
+  log_debug("Creating vector");
+  status &= otto_vector_from_array(data, len, sizeof(uint32_t), &vec);
+
+  log_debug("Extending vector");
+  uint32_t values[] = {20, 19, 15};
+  size_t values_len = 3;
+  status &= otto_vector_extend_array(&values, &vec);
+
+  log_debug("Checking that the dimensions changed properly");
+  if (vec.size != 11 || vec.capacity != 11) {
+    return TEST_FAIL;
+  }
+
+  log_debug("Checking border scenario");
+  if (otto_vector_extend_array(NULL, 10010, &vec) != OTTO_STATUS_FAILURE) {
+    return TEST_FAIL;
+  }
+  log_debug("Checking that the failed extend does not modify the vector");
+  if (vec.size != 11 || vec.capacity != 11) {
+    return TEST_FAIL;
+  }
+
+  log_debug("Checking that the extend happened fine");
+  uint32_t contained_val;
+  for (int i = 0; i < values_len; i++) {
+    status &= otto_vector_get(&vec, i + len, &contained_val);
+    if (contained_val != values[i]) {
+      return TEST_FAIL;
+    }
+  }
+
+  if (status == OTTO_STATUS_FAILURE) {
+    return TEST_FAIL;
+  }
+  return TEST_PASS;
+}
+
 int main() {
   OTTO_CALL_TEST(test_push);
   return TEST_PASS;
