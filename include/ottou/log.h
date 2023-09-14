81670b856639b0cac2e68b38efc784025fd918b0
Cristóbal Allendes <callendes.molina@gmail.com>
2023-09-14 17:28:39 -0300

    Added an extern guard

--- include/ottou/log.h
+++ include/ottou/log.h
@@ -13,6 +13,10 @@
 #include <stdio.h>
 #include <time.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define LOG_VERSION "0.1.0"
 #define __FILENAME__ (&__FILE__[SOURCE_PATH_SIZE])
 
@@ -46,5 +50,9 @@ int log_add_callback(log_LogFn fn, void *udata, int level);
 int log_add_fp(FILE *fp, int level);
 
 void log_log(int level, const char *file, int line, const char *fmt, ...);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
