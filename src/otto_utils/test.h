8b066114dd97a25ff2e9058a5ecb2a20b319296e
Cristóbal Allendes <callendes.molina@gmail.com>
2023-09-13 23:41:02 -0300

    Removed remnants from the source where I copied the code from

--- src/otto_utils/test.h
+++ src/otto_utils/test.h
@@ -5,35 +5,29 @@
 #define TEST_FAIL 1
 #define TEST_PASS 0
 
-#define otto_assert(cond)                                                     \
+#define otto_assert(cond)                                                      \
   if (!(cond))                                                                 \
   return TEST_FAIL
-#define otto_assert_false(cond)                                               \
+#define otto_assert_false(cond)                                                \
   if (cond)                                                                    \
   return TEST_FAIL
-#define otto_assert_eqi(v1, v2)                                               \
+#define otto_assert_eqi(v1, v2)                                                \
   if ((v1) != (v2)) {                                                          \
     log_debug("TEST_FAIL: Expected %i, found %i", v2, v1);                     \
     return TEST_FAIL;                                                          \
   }
-#define otto_assert_nei(v1, v2)                                               \
+#define otto_assert_nei(v1, v2)                                                \
   if ((v1) == (v2)) {                                                          \
     log_debug("TEST_FAIL: Expected different than %i, found %i", v2, v1);      \
     return TEST_FAIL;                                                          \
   }
-#define otto_assert_streq(v1, v2)                                             \
+#define otto_assert_streq(v1, v2)                                              \
   if (strcmp((v1), (v2))) {                                                    \
     log_debug("TEST_FAIL: Expected %s, found %s", v2, v1);                     \
     return TEST_FAIL;                                                          \
   }
-#define otto_assert_strne(v1, v2)                                             \
+#define otto_assert_strne(v1, v2)                                              \
   if (!strcmp((v1), (v2))) {                                                   \
     log_debug("TEST_FAIL: Expected different than %s, found %s", v2, v1);      \
     return TEST_FAIL;                                                          \
   }
-#define otto_assert_chromeq(v1, v2)                                           \
-  if (!otto_ch_equal(&(v1), &(v2)))                                           \
-  return TEST_FAIL
-#define otto_assert_chromne(v1, v2)                                           \
-  if (otto_ch_equal(&(v1), &(v2)))                                            \
-  return TEST_FAIL
