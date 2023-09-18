0764b0a4c8ae126ac8bbdc44375d400e50017d09
Cristóbal Allendes <callendes.molina@gmail.com>
2023-09-18 00:52:16 -0300

    Moved the flags to the end2

--- include/otto/vector.h
+++ include/otto/vector.h
@@ -56,8 +56,9 @@ otto_status_t otto_vector_extend_array(otto_vector_t *vec, const void *src,
 
 /* Vector movements */
 
-otto_status_t otto_vector_register(otto_vector_t *vec, cl_mem_flags flags,
-                                   const otto_runtime_t *ctx);
+otto_status_t otto_vector_register(otto_vector_t *vec,
+                                   const otto_runtime_t *ctx,
+                                   const cl_mem_flags flags);
 
 #ifdef __cplusplus
 }
