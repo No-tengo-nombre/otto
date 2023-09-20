dd9f2e5d374af1af47c5fbaee127cc941c7cb04a
Cristóbal Allendes <callendes.molina@gmail.com>
2023-09-20 17:50:35 -0300

    Commented out the call that uses arrays

--- include/otto/cl/kernel.h
+++ include/otto/cl/kernel.h
@@ -23,7 +23,7 @@ otto_status_t otto_kernel_new(const otto_program_t *prog, const char *name,
                               const size_t nargs, otto_runtime_t *ctx,
                               otto_kernel_t *out);
 
-otto_status_t otto_kernel_call(const otto_kernel_t *ker, void **args);
+// otto_status_t otto_kernel_call(const otto_kernel_t *ker, void **args);
 otto_status_t otto_kernel_vcall(const otto_kernel_t *ker, ...);
 
 #ifdef __cplusplus
