c510be75075070413db23db3c3686c5f51a8d961
Cristóbal Allendes <callendes.molina@gmail.com>
2023-10-05 16:20:24 -0300

    Removed a # from __VA_ARGS__ to remove an annoying error

--- src/otto_utils/macros.h
+++ src/otto_utils/macros.h
@@ -6,7 +6,7 @@
 
 #define OTTO_CALL_I(x, msg, ...)                                               \
   if (x != OTTO_STATUS_SUCCESS) {                                              \
-    logi_error(msg, ##__VA_ARGS__);                                            \
+    logi_error(msg, #__VA_ARGS__);                                             \
     return OTTO_STATUS_FAILURE;                                                \
   }
 
@@ -14,7 +14,7 @@
   {                                                                            \
     cl_int err_ = x;                                                           \
     if (err_ != CL_SUCCESS) {                                                  \
-      logi_error(msg " (%d)", ##__VA_ARGS__, err_);                            \
+      logi_error(msg " (%d)", #__VA_ARGS__, err_);                             \
       return OTTO_STATUS_FAILURE;                                              \
     }                                                                          \
   }
