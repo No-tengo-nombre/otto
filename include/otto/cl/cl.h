4eb6269fcf9b20fbe9a82c6b58774935f8a501a2
Cristóbal Allendes <callendes.molina@gmail.com>
2023-10-05 22:05:00 -0300

    Added the option to specify the CL version with the macro OTTO_CL_VERSION

--- include/otto/cl/cl.h
+++ include/otto/cl/cl.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#ifdef OTTO_CL_VERSION
+#define CL_TARGET_OPENCL_VERSION OTTO_CL_VERSION
+#else
 #define CL_TARGET_OPENCL_VERSION 300
+#endif
 
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
