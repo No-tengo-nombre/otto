e7e98da0496d4095251126f716b201b81c8b6c22
Cristóbal Allendes <callendes.molina@gmail.com>
2023-10-18 18:02:45 -0300

    Made the enums explicit

--- pysrc/otto/ffi/devices.py
+++ pysrc/otto/ffi/devices.py
@@ -1,5 +1,8 @@
 CTYPEDEF = """
-typedef ... otto_device_t;
+typedef enum otto_device {
+  OTTO_DEVICE_CPU,
+  OTTO_DEVICE_GPU,
+} otto_device_t;
 """
 
 CDEF = """
--- pysrc/otto/ffi/status.py
+++ pysrc/otto/ffi/status.py
@@ -1,5 +1,8 @@
 CTYPEDEF = """
-typedef ... otto_status_t;
+typedef enum otto_status {
+  OTTO_STATUS_SUCCESS = 0,
+  OTTO_STATUS_FAILURE = 1,
+} otto_status_t;
 """
 
 CDEF = """
