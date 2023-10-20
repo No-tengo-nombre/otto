25310a2c73024d2d2755d3fbfac55561852b8117
Cristóbal Allendes <callendes.molina@gmail.com>
2023-10-20 13:26:10 -0300

    Made the example test if it imports correctly

--- pyexamples/ffi.py
+++ pyexamples/ffi.py
@@ -2,9 +2,10 @@ from pathlib import Path
 import os
 from otto.defs import ROOT_PATH
 
-os.add_dll_directory(str(Path.joinpath(ROOT_PATH, "pysrc")))
-import _otto
+# os.add_dll_directory(str(Path.joinpath(ROOT_PATH, "pysrc")))
+from _otto import lib as otto
 
 
 def main():
-    pass
+    a = otto.otto_vector_new(5, None) # This fails but I only care that it works
+    print(a)
