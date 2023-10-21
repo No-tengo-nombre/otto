edbd567eb6fc8f64026fb690bc9ffe3fff83f3a5
Cristóbal Allendes <callendes.molina@gmail.com>
2023-10-21 00:22:04 -0300

    Made the part that adds the dir to the shared library path also add it to the trusted DLL directories in Windows

--- pysrc/otto_ffi/__init__.py
+++ pysrc/otto_ffi/__init__.py
@@ -11,3 +11,8 @@ try:
         lib_path}: {os.environ["LD_LIBRARY_PATH"]}"
 except KeyError:
     os.environ["LD_LIBRARY_PATH"] = lib_path
+
+# Add the dll directory to the trusted path
+_WINDOWS_NAME = "nt"
+if os.name == _WINDOWS_NAME:
+    os.add_dll_directory(os.environ["LD_LIBRARY_PATH"])
