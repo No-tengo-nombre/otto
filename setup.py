0d33ba36548365cc39dfa824ad5befd146dbf6fb
Cristóbal Allendes <callendes.molina@gmail.com>
2023-10-20 21:26:01 -0300

    Added a comment to the setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
 
+# The setup assumes the C library has been built and the .dll/.so files are located
+# in "bin" for Windows and "lib" for Linux
+
 if __name__ == "__main__":
     setup()
