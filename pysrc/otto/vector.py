539a25b385ea6bd33fae2b02a1ed9f5cee4fbbe0
Cristóbal Allendes <callendes.molina@gmail.com>
2023-10-21 17:21:28 -0300

    Added extension from list

--- pysrc/otto/vector.py
+++ pysrc/otto/vector.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Self
 
 from otto.dtypes import DataType
 
@@ -6,7 +6,7 @@ import otto_ffi as _
 from _otto import ffi, lib as _ottol
 
 
-class Vector:
+class Vector[T]:
     __slots__ = ("_cdata", "_dtype", "_dsize", "_index")
 
     def __init__(self, dtype: DataType):
@@ -52,23 +52,26 @@ class Vector:
     def __str__(self) -> str:
         return f"Vector<{self._dtype.name}>[{self.len}/{self.capacity}]"
 
-    def __getitem__(self, key: int):
+    def __getitem__(self, key: int) -> T:
         return self.get(key)
 
-    def __setitem__(self, key: int, value):
+    def __setitem__(self, key: int, value: T):
         return self.set_key(key, value)
 
-    def __iter__(self):
+    def __iter__(self) -> Self:
         self._index = 0
         return self
 
-    def __next__(self):
+    def __next__(self) -> T:
         try:
             val = self[self._index]
         except IndexError:
             raise StopIteration
         self._index += 1
         return val
+
+    def __len__(self) -> int:
+        return self.len
 
     def string(self) -> str:
         return self.__str__()
@@ -82,7 +85,7 @@ class Vector:
                 f"index {idx % self.len} out of range for vector of len {self.len}")
         return idx % self.len
 
-    def get(self, key: int):
+    def get(self, key: int) -> T:
         key = self.validate_index(key)
         val = ffi.new(f"{self._dtype.long_name} *")
         _ottol.otto_vector_get(self._cdata, key, val)
@@ -94,7 +97,20 @@ class Vector:
         val[0] = value
         _ottol.otto_vector_set(self._cdata, key, val)
 
-    def to_list(self) -> List:
+    def to_list(self) -> List[T]:
         return list(self)
 
     # TODO: Implement interpreting as different collections (e.g. numpy arrays)
+
+    def resize(self, new_capacity: int) -> None:
+        _ottol.otto_vector_resize(self._cdata, new_capacity)
+
+    def push(self, element: T) -> None:
+        val = ffi.new(f"{self._dtype.long_name} *")
+        val[0] = element
+        _ottol.otto_vector_push(self._cdata, val)
+
+    def extend_from_list(self, target: List[T]):
+        size = len(target)
+        val = ffi.new(f"{self._dtype.long_name}[]", target)
+        _ottol.otto_vector_extend_array(self._cdata, val, size)
