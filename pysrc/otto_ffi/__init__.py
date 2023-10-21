4ee595901ee7773ab742da13cee089d12d9dc3e2
Cristóbal Allendes <callendes.molina@gmail.com>
2023-10-21 00:04:24 -0300

    Moved the ld_path component to the __init__

--- pysrc/otto_ffi/__init__.py
+++ pysrc/otto_ffi/__init__.py
@@ -1,1 +1,13 @@
 """FFI for the otto C functions"""
+
+
+import os
+from pathlib import Path
+
+
+lib_path = str(Path(__file__).parent)
+try:
+    os.environ["LD_LIBRARY_PATH"] = f"{
+        lib_path}: {os.environ["LD_LIBRARY_PATH"]}"
+except KeyError:
+    os.environ["LD_LIBRARY_PATH"] = lib_path
--- pysrc/otto_ffi/ld_path.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-
-
-lib_path = str(Path(__file__).parent)
-try:
-    os.environ["LD_LIBRARY_PATH"] = f"{lib_path}:{os.environ["LD_LIBRARY_PATH"]}"
-except KeyError:
-    os.environ["LD_LIBRARY_PATH"] = lib_path
