66764c03a8e5a9c8ce8956f7c2ba305d3c08bde2
Cristóbal Allendes <callendes.molina@gmail.com>
2023-10-31 01:54:19 -0300

    Added functions that interpret OpenCL error codes as strings2

--- include/ottou/macros.h
+++ include/ottou/macros.h
@@ -4,6 +4,8 @@
 #include <otto/cl/cl.h>
 #include <otto/status.h>
 
+const char *get_cl_error_msg(cl_int err);
+
 #define OTTO_CALL(x, msg, ...)                                                 \
   if (x != OTTO_STATUS_SUCCESS) {                                              \
     log_error(msg, #__VA_ARGS__);                                              \
@@ -14,7 +16,7 @@
   {                                                                            \
     cl_int err_ = x;                                                           \
     if (err_ != CL_SUCCESS) {                                                  \
-      log_error(msg " (%d)", #__VA_ARGS__, err_);                              \
+      log_error(msg " [%d](%s)", #__VA_ARGS__, err_, get_cl_error_msg(err_));  \
       return OTTO_STATUS_FAILURE;                                              \
     }                                                                          \
   }
--- src/otto_utils/macros.h
+++ src/otto_utils/macros.h
@@ -3,6 +3,7 @@
 #include "vendor/log.h"
 #include <otto/cl/cl.h>
 #include <otto/status.h>
+#include <ottou/macros.h>
 
 #define OTTO_CALL_I(x, msg, ...)                                               \
   if (x != OTTO_STATUS_SUCCESS) {                                              \
@@ -14,7 +15,7 @@
   {                                                                            \
     cl_int err_ = x;                                                           \
     if (err_ != CL_SUCCESS) {                                                  \
-      logi_error(msg " (%d)", #__VA_ARGS__, err_);                             \
+      logi_error(msg " [%d](%s)", #__VA_ARGS__, err_, get_cl_error_msg(err_)); \
       return OTTO_STATUS_FAILURE;                                              \
     }                                                                          \
   }
