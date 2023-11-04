cd7ae7032ca17dfacaf62d9fd21b04f279d07c2f
Cristóbal Allendes <callendes.molina@gmail.com>
2023-11-03 21:09:28 -0300

    Modified the example a bit

--- examples/python/runtimes.py
+++ examples/python/runtimes.py
@@ -1,6 +1,10 @@
-from otto.cl.runtime import Runtime, Kernels
+from otto.cl.runtime import Runtime, Kernels, Device
 
 
 def main():
-    a = Runtime(kernels=Kernels.ALL)
+    a = Runtime(device=Device.GPU, kernels=Kernels.ALL)
     b = Runtime()
+    if id(a) != id(b):
+        print("Something went wrong")
+    else:
+        print("All good!")
