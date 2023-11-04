2c10b969d81f2eb8afcdcd5c092d8881a10ce7f2
Cristóbal Allendes <callendes.molina@gmail.com>
2023-11-04 02:11:21 -0300

    Made the example use the kernels

--- examples/python/vector_add.py
+++ examples/python/vector_add.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from otto.cl import Runtime, Device
+from otto.cl import Runtime, Device, KernelArgs
 from otto import Vector
 from otto_utils.logger import LOGGER
 
@@ -10,8 +10,16 @@ def main(size=256):
 
     LOGGER.info("Setting runtime parameters")
     Runtime.cls_device = Device.GPU
+    ctx = Runtime()
 
     LOGGER.info("Creating vectors")
     a = Vector.from_numpy(np.arange(size)).to_device()
     b = Vector.from_numpy(size - np.arange(size)).to_device()
-    out = Vector.with_capacity(size, a._dtype).set_write().to_device()
+    print(f"a = {a.content_string()}")
+    print(f"b = {b.content_string()}")
+
+    hparams = KernelArgs(1, size, 64)
+    out = ctx.call_binop_kernel_no_out("otto_vector_add", a, b, hparams)
+
+    out.to_host()
+    print(f"out = {out.content_string()}")
