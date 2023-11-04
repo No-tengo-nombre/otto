598ed2e074deb0094a0f46b6d7325f35110682be
Cristóbal Allendes <callendes.molina@gmail.com>
2023-11-03 22:15:38 -0300

    Updated the FFI

--- pysrc/otto_ffi/ffi/_vector.py
+++ pysrc/otto_ffi/ffi/_vector.py
@@ -36,8 +36,18 @@ otto_status_t otto_vector_extend_array(otto_vector_t *vec, const void *src,
                                        const size_t len);
 
 
+otto_status_t otto_vector_setread(otto_vector_t *vec);
+otto_status_t otto_vector_setwrite(otto_vector_t *vec);
+otto_status_t otto_vector_setreadwrite(otto_vector_t *vec);
+
+otto_status_t otto_vector_todevice_mode(otto_vector_t *vec,
+                                        const otto_runtime_t *ctx,
+                                        const cl_mem_flags flags);
 otto_status_t otto_vector_todevice(otto_vector_t *vec,
-                                   const otto_runtime_t *ctx,
-                                   const cl_mem_flags flags);
+                                   const otto_runtime_t *ctx);
+otto_status_t otto_vector_todevice_read(otto_vector_t *vec,
+                                        const otto_runtime_t *ctx);
+otto_status_t otto_vector_todevice_write(otto_vector_t *vec,
+                                         const otto_runtime_t *ctx);
 otto_status_t otto_vector_tohost(otto_vector_t *vec, uint64_t total);
 """
