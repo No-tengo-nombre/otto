d0704f7ec2535dcf3b7275abfd1e1c18662f840e
Cristóbal Allendes <callendes.molina@gmail.com>
2024-01-02 14:48:03 -0300

    Added a basic structure for a tensor

---
+++ include/otto/tensor.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "status.h"
+#include "vector.h"
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Tensor object that generalizes a vector to any rank.
+ *
+ * Tensors are designed to contain otto vectors while providing abstractions
+ * that facilitate doing mathematical work with them. They wrap some contiguous
+ * data, while containing higher level information about what said data
+ * represents (e.g. nine contiguous floats can correspond to a length 9 vector
+ * or a 3x3 matrix).
+ *
+ * The `shape` attribute corresponds to an array of `rank` elements describing
+ * the shape of the underlying data.
+ */
+typedef struct otto_tensor {
+  otto_vector_t *vec;
+  size_t rank;
+  size_t *shape;
+} otto_tensor_t;
+
+/* Tensor creation */
+
+otto_status_t otto_tensor_new(otto_tensor_t *out);
+otto_status_t otto_tensor_from_vector(otto_vector_t *vec, const size_t rank,
+                                      const int64_t *shape);
+
+/* Tensor attributes */
+
+otto_status_t otto_tensor_get(const otto_tensor_t *vec, const size_t *idx,
+                              void *out);
+otto_status_t otto_tensor_set(otto_tensor_t *vec, const size_t *idx,
+                              const void *src);
+
+#ifdef __cplusplus
+}
+#endif
