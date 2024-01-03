68d2b593087b94f89a04defb4c8081a297fff5c1
Cristóbal Allendes <callendes.molina@gmail.com>
2024-01-03 17:48:38 -0300

    Implemented getting and initializing as zero

--- include/otto/tensor.h
+++ include/otto/tensor.h
@@ -29,16 +29,16 @@ typedef struct otto_tensor {
 /* Tensor creation */
 
 // otto_status_t otto_tensor_new(otto_tensor_t *out);
-otto_status_t otto_tensor_zero(const size_t rank, const size_t *shape,
+otto_status_t otto_tensor_zero(const size_t rank, size_t *shape,
                                const size_t data_size, otto_tensor_t *out);
 // otto_status_t otto_tensor_from_vector(otto_vector_t *vec, const size_t rank,
 //                                       const int64_t *shape);
 
 /* Tensor attributes */
 
-otto_status_t otto_tensor_get(const otto_tensor_t *vec, const size_t *idx,
+otto_status_t otto_tensor_get(const otto_tensor_t *mat, const size_t *idx,
                               void *out);
-otto_status_t otto_tensor_set(otto_tensor_t *vec, const size_t *idx,
+otto_status_t otto_tensor_set(otto_tensor_t *mat, const size_t *idx,
                               const void *src);
 
 #ifdef __cplusplus
--- src/otto_core/tensor.c
+++ src/otto_core/tensor.c
@@ -2,6 +2,7 @@
 #include <otto/tensor.h>
 #include <otto/vector.h>
 #include <otto_utils/macros.h>
+#include <otto_utils/vendor/log.h>
 
 #include <stdint.h>
 
@@ -15,19 +16,48 @@
 //   return OTTO_STATUS_SUCCESS;
 // }
 
-otto_status_t otto_tensor_zero(const size_t rank, const size_t *shape,
+otto_status_t otto_tensor_zero(const size_t rank, size_t *shape,
                                const size_t data_size, otto_tensor_t *out) {
-  size_t len = *shape;
-  for (int i = 0; i < rank; i++, ++shape) {
-    len *= *shape;
+  size_t len = 1;
+  size_t *original_shape = shape;
+  for (int i = 0; i < rank; i++) {
+    len *= *shape++;
   }
+  logi_debug("Length for underlying vector is %i", len);
   otto_vector_t vec;
   OTTO_CALL_I(otto_vector_zero(len, data_size, &vec), "Failed creating vector");
   otto_tensor_t result = {
       .vec = vec,
       .rank = rank,
-      .shape = shape,
+      .shape = original_shape,
   };
   *out = result;
   return OTTO_STATUS_SUCCESS;
+}
+
+otto_status_t otto_tensor_get(const otto_tensor_t *mat, const size_t *idx,
+                              void *out) {
+  size_t index = 0;
+  const size_t *shapes = mat->shape;
+  for (int k = 0; k < mat->rank; k++) {
+    size_t s = 1;
+    for (int n = k + 1; n < mat->rank; n++) {
+      s *= shapes[n];
+    }
+    logi_debug("s %i", s);
+    index += s * (*idx++);
+  }
+  if (index < 0 || index >= mat->vec.len) {
+    logi_error("Index %i out of bounds", index);
+    return OTTO_STATUS_FAILURE("Index out of bounds");
+  }
+  logi_debug("Getting index %i", index);
+  OTTO_CALL_I(otto_vector_get(&mat->vec, index, out),
+              "Failed getting index from underlying vector");
+  return OTTO_STATUS_SUCCESS;
+}
+
+otto_status_t otto_tensor_set(otto_tensor_t *mat, const size_t *idx,
+                              const void *src) {
+  return OTTO_STATUS_FAILURE("Not implemented");
 }
