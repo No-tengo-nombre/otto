263e95619c90dd16d8b2ad14e20b9b91c540686c
Cristóbal Allendes <callendes.molina@gmail.com>
2024-01-03 17:55:42 -0300

    Separated file

--- src/otto_core/tensor_attrs.c
+++ src/otto_core/tensor_attrs.c
@@ -19,35 +19,6 @@ size_t calculate_index(const size_t *idx, const size_t rank,
   return index;
 }
 
-// otto_status_t otto_tensor_new(otto_tensor_t *out) {
-//   otto_tensor_t result = {
-//       .vec = NULL,
-//       .rank = 0,
-//       .shape = NULL,
-//   };
-//   *out = result;
-//   return OTTO_STATUS_SUCCESS;
-// }
-
-otto_status_t otto_tensor_zero(const size_t rank, size_t *shape,
-                               const size_t data_size, otto_tensor_t *out) {
-  size_t len = 1;
-  size_t *original_shape = shape;
-  for (int i = 0; i < rank; i++) {
-    len *= *shape++;
-  }
-  logi_debug("Length for underlying vector is %i", len);
-  otto_vector_t vec;
-  OTTO_CALL_I(otto_vector_zero(len, data_size, &vec), "Failed creating vector");
-  otto_tensor_t result = {
-      .vec = vec,
-      .rank = rank,
-      .shape = original_shape,
-  };
-  *out = result;
-  return OTTO_STATUS_SUCCESS;
-}
-
 otto_status_t otto_tensor_get(const otto_tensor_t *mat, const size_t *idx,
                               void *out) {
   size_t index = calculate_index(idx, mat->rank, mat->shape);
---
+++ src/otto_core/tensor_creation.c
@@ -0,0 +1,36 @@
+#include <otto/status.h>
+#include <otto/tensor.h>
+#include <otto/vector.h>
+#include <otto_utils/macros.h>
+#include <otto_utils/vendor/log.h>
+
+#include <stdint.h>
+
+// otto_status_t otto_tensor_new(otto_tensor_t *out) {
+//   otto_tensor_t result = {
+//       .vec = NULL,
+//       .rank = 0,
+//       .shape = NULL,
+//   };
+//   *out = result;
+//   return OTTO_STATUS_SUCCESS;
+// }
+
+otto_status_t otto_tensor_zero(const size_t rank, size_t *shape,
+                               const size_t data_size, otto_tensor_t *out) {
+  size_t len = 1;
+  size_t *original_shape = shape;
+  for (int i = 0; i < rank; i++) {
+    len *= *shape++;
+  }
+  logi_debug("Length for underlying vector is %i", len);
+  otto_vector_t vec;
+  OTTO_CALL_I(otto_vector_zero(len, data_size, &vec), "Failed creating vector");
+  otto_tensor_t result = {
+      .vec = vec,
+      .rank = rank,
+      .shape = original_shape,
+  };
+  *out = result;
+  return OTTO_STATUS_SUCCESS;
+}
